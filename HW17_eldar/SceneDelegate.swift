7f4e9e413dcecd5e043f36cd25fe2c2e513bc995
Eldar Garbuzov <eldar.garbuzov@gmail.com>
2022-06-15 18:42:27 +0300

    add keychain password storage
    password storage via keychain and removed previous password solutions

--- HW17_eldar/SceneDelegate.swift
+++ HW17_eldar/SceneDelegate.swift
@@ -6,17 +6,17 @@
 //
 
 import UIKit
+import KeychainSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
     var passwordTextField = UITextField()
-    var passwordValue: String? = nil
+    
+    let keychain = KeychainSwift()
+    
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window?.windowScene = windowScene
@@ -24,30 +24,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController
         let navigationVC  = UINavigationController(rootViewController: viewController!)
-        //        UINavigationBar.appearance().barTintColor = .gray
-        //        UINavigationBar.appearance().tintColor = .black
         window?.rootViewController = navigationVC
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
         passSetOrNot()
-
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
     
     func sceneDidBecomeActive(_ scene: UIScene) {
         passSetOrNot()
-        
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
     
     func sceneWillEnterForeground(_ scene: UIScene) {
@@ -61,9 +49,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func passSetOrNot() {
-        if passwordValue != nil {
+        if keychain.get("password") != nil  {
             passwordIsOnAlert()
-        } else if passwordValue == nil  {
+        } else  {
             noPasswordAlert()
         }
     }
@@ -71,19 +59,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func noPasswordAlert() {
         let passwordAlert = UIAlertController(title: "Set a password?", message: "Choose options", preferredStyle: .alert)
         let setPassword  = UIAlertAction(title: "Create password", style: .default) { _ in
-            let passwordSetting = UIAlertController(title: "Set a password", message: "Create password using any characters", preferredStyle: .alert)
-            passwordSetting.addTextField() { textField in
-                self.passwordTextField = textField
-            }
-            let confirmSettingPassword = UIAlertAction(title: "Save", style: .default) { _ in
-                self.passwordValue = self.passwordTextField.text!
-            }
-            passwordSetting.addAction(confirmSettingPassword)
-            self.window?.rootViewController?.present(passwordSetting, animated: true)
-            if self.passwordValue != self.passwordTextField.text {
-                let errorAlert = UIAlertController(title: "Access denied", message: "Sorry, but password is incorrect", preferredStyle: .actionSheet)
-                self.window?.rootViewController?.present(errorAlert, animated: true)
-            }
+            self.setPass()
+            print(self.keychain.get("password"))
         }
         let cancelAlert = UIAlertAction(title: "Dismiss", style: .default)
         passwordAlert.addAction(setPassword)
@@ -97,7 +74,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             self.passwordTextField = textField
         }
         let confirmPasswordErorAlert = UIAlertAction(title: "Confirm", style: .default) { _ in
-            if self.passwordTextField.text != self.passwordValue {
+            if self.passwordTextField.text != self.keychain.get("password") {
                 let incorrectPasswordWorning = UIAlertController(title: "Password is incorrect", message: "Sorry, try again", preferredStyle: .alert)
                 self.passSetOrNot()
                 self.window?.rootViewController?.present(incorrectPasswordWorning, animated: true)
@@ -115,5 +92,38 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window?.rootViewController?.present(accessPermittedAlert, animated: true)
         accessPermittedAlert.dismiss(animated: true)
     }
+    //пустое поле
+    func emptyWorning() {
+        let imptyWorningAlert = UIAlertController(title: "Uups!", message: "All fields should be filled", preferredStyle: .alert)
+        let okImptyWorningAlert = UIAlertAction(title: "OK", style: .cancel) { _ in
+            self.passSetOrNot()
+        }
+        imptyWorningAlert.addAction(okImptyWorningAlert)
+        self.window?.rootViewController?.present(imptyWorningAlert, animated: true)
+        
+    }
+    
+    func setPass() {
+        let passwordSetting = UIAlertController(title: "Set a password", message: "Create password using any characters", preferredStyle: .alert)
+   
+        let confirmSettingPassword = UIAlertAction(title: "Save", style: .default) { _ in
+            
+            guard passwordSetting.textFields?.first?.text != "" else { return self.emptyWorning() }
+            guard let passwordText = passwordSetting.textFields?.first?.text else { return }
+            self.keychain.set(passwordText, forKey: "password")
+            
+//            guard let textPassword = self.passwordTextField.text else { return }
+//            self.keychain.set(textPassword, forKey: "password")
+            //                self.passwordValue = self.passwordTextField.text!
+        }
+        passwordSetting.addTextField()
+        passwordSetting.addAction(confirmSettingPassword)
+        self.window?.rootViewController?.present(passwordSetting, animated: true)
+        //            if self.keychain.get("password") != self.passwordTextField.text {
+        //                let errorAlert = UIAlertController(title: "Access denied", message: "Sorry, but password is incorrect", preferredStyle: .actionSheet)
+        //                self.window?.rootViewController?.present(errorAlert, animated: true)
+        //            }
+    }
+    
 } // конец класса
 
